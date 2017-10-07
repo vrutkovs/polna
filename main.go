9e81c244ac783efd6fe5129bbb0d997500642f3c
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-10-07 11:38:13 +0200

    Split io parts into a separate module

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
     "fmt"
     "io/ioutil"
-    "math/rand"
     "net/http"
-    "os"
-    "time"
 
     "html/template"
     "path/filepath"
 
-    "github.com/gin-contrib/multitemplate"
     "github.com/gin-contrib/location"
+    "github.com/gin-contrib/multitemplate"
     "github.com/gin-gonic/gin"
 )
 
@@ -43,60 +40,6 @@ func loadTemplates(templatesDir string) multitemplate.Render {
         r.Add(filepath.Base(layout), template.Must(template.ParseFiles(files...)))
     }
     return r
-}
-
-func generateID() string {
-    t := time.Now().UTC()
-    entropy := rand.New(rand.NewSource(t.UnixNano()))
-    
-    id := fmt.Sprintf("%X%X", t.Nanosecond(), entropy.Int())
-
-    // Check that this ID doesn't exist yet.Int()
-    filepath, err := filepath.Abs(uploadDir + id)
-    if err != nil {
-        return generateID()
-    }
-
-    if _, err := os.Stat(filepath); os.IsNotExist(err) {
-        return id
-    }
-    return generateID()
-}
-
-func getPaste(id string) (string, error) {
-    filepath, err := filepath.Abs(uploadDir + id)
-    if err != nil {
-        panic(err.Error())
-    }
-
-    if _, err := os.Stat(filepath); os.IsNotExist(err) {
-        return "", err
-    }
-
-    bs, err := ioutil.ReadFile(filepath)
-    if err != nil {
-        return "", err
-    }
-    return string(bs), nil
-}
-
-func savePasteInFile(paste string) (string, error) {
-    // Generate ID
-    id := generateID()
-
-    filepath, err := filepath.Abs(uploadDir + id)
-    if err != nil {
-        return "", err
-    }
-
-    file, err := os.Create(filepath)
-    if err != nil {
-        return "", err
-    }
-    defer file.Close()
-
-    file.WriteString(paste)
-    return id, nil
 }
 
 func addRoutes(r *gin.Engine) {
---
+++ polna.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func generateID() string {
+    t := time.Now().UTC()
+    entropy := rand.New(rand.NewSource(t.UnixNano()))
+
+    id := fmt.Sprintf("%X%X", t.Nanosecond(), entropy.Int())
+
+    // Check that this ID doesn't exist yet.Int()
+    filepath, err := filepath.Abs(uploadDir + id)
+    if err != nil {
+        return generateID()
+    }
+
+    if _, err := os.Stat(filepath); os.IsNotExist(err) {
+        return id
+    }
+    return generateID()
+}
+
+func getPaste(id string) (string, error) {
+    filepath, err := filepath.Abs(uploadDir + id)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    if _, err := os.Stat(filepath); os.IsNotExist(err) {
+        return "", err
+    }
+
+    bs, err := ioutil.ReadFile(filepath)
+    if err != nil {
+        return "", err
+    }
+    return string(bs), nil
+}
+
+func savePasteInFile(paste string) (string, error) {
+    // Generate ID
+    id := generateID()
+
+    filepath, err := filepath.Abs(uploadDir + id)
+    if err != nil {
+        return "", err
+    }
+
+    file, err := os.Create(filepath)
+    if err != nil {
+        return "", err
+    }
+    defer file.Close()
+
+    file.WriteString(paste)
+    return id, nil
+}
